0274c96a9c2e1f404a12e63ed20963ea0b71d7c6
Vardan Petrosyan <petrosyanvardan2009@gmail.com>
2024-04-30 00:12:34 +0400

    Added some image filters in main.c

--- test.c
+++ test.c
@@ -4,22 +4,68 @@
 #include "src/bmp.c"
 
 
-//testing Bitmap library
-// taking image file as input and grayscaling it
-int main()
-{
-    BMPImage img = readBMP("tests/0.bmp");
+unsigned char clamp(int value, int min, int max) {
+    if (value < min)
+        return (unsigned char)min;
+    else if (value > max)
+        return (unsigned char)max;
+    else
+        return (unsigned char)value;
+}
 
-    for (int y = 0; y < img.infoHeader.height; y++) {
-        for (int x = 0; x < img.infoHeader.width; x++) {
-            Pixel p = getPixel(&img,x,y);
+
+//testing Bitmap library
+
+// taking image as input and grayscaling it
+void grayscale(BMPImage* img)
+{
+    for (int y = 0; y < img->infoHeader.height; y++) {
+        for (int x = 0; x < img->infoHeader.width; x++) {
+            Pixel p = getPixel(img,x,y);
             unsigned char pixavg = (p.r+p.g+p.b)/3;
             p.r = pixavg;
             p.g = pixavg;
             p.b = pixavg;
-            setPixel(&img,x,y,p);
+            setPixel(img,x,y,p);
         }
     }
+}
+
+// taking image as input and adding brightness to each pixel
+void invert(BMPImage* img)
+{
+    for (int y = 0; y < img->infoHeader.height; y++) {
+        for (int x = 0; x < img->infoHeader.width; x++) {
+            Pixel p = getPixel(img,x,y);
+            p.r = 255 - p.r;
+            p.g = 255 - p.g;
+            p.b = 255 - p.b;
+            setPixel(img,x,y,p);
+        }
+    }
+}
+
+void vintage(BMPImage* img)
+{
+    for (int y = 0; y < img->infoHeader.height; y++) {
+        for (int x = 0; x < img->infoHeader.width; x++) {
+            Pixel p = getPixel(img, x, y);
+
+            p.b = (unsigned char)(p.b * 0.5);
+            p.g = (unsigned char)(p.g * 0.7);
+
+            setPixel(img, x, y, p);
+        }
+    }
+}
+
+
+int main()
+{
+    BMPImage img = readBMP("tests/0.bmp");
+
+    grayscale(&img);
+    vintage(&img);
 
     writeBMP("tests/0out.bmp",img);
 }
